b9f99aa056127f4772b6e1f88595bdc390261357
JasonSYuen <js99168@icloud.com>
2024-03-09 19:26:43 -0800

    m

--- webpage.js
+++ webpage.js
@@ -1,7 +1,7 @@
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { doc, getDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
