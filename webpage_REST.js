f58063aa18d27c9a23356f65fdf114c349a90153
JasonSYuen <js99168@icloud.com>
2024-03-09 20:13:00 -0800

    Create webpage_REST.js

---
+++ webpage_REST.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("login").addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+
+
+
+});
