c268c96f030aa3af65a1e31802cca8debbc20507
JasonSYuen <js99168@icloud.com>
2024-03-09 21:06:13 -0800

    rest api

--- webpage_REST.js
+++ webpage_REST.js
@@ -2,11 +2,28 @@
 
 document.getElementById("login").addEventListener("submit", function (event) {
     event.preventDefault()
-
+    console.log("a")
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://jasony33.pythonanywhere.com/grades');
+    console.log(request)
+    request.onload = function () {
+        const ourData = JSON.parse(request.responseText);
+        console.log(request.responseText);
+        console.log(request)
+        console.log(ourData)
+        console.log("b")
+    }
+    console.log("c")
+
+    request.send();
 
 
 
 });
+
+
+
+
